deb2c152e38eeae6f4d434cc2ecc4c2de7765648
Valentin Serra <serval.core@gmail.com>
2021-10-13 14:22:35 +0100

    React tutorial basics. Working on the albums website.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// setup vars
+const albums = [
+  {
+    id: 1,
+    author: 'The Naturals',
+    name: 'On The Way (To the Laughing Light Of Plenty)',
+    cover: 'https://f4.bcbits.com/img/a1321283216_16.jpg',
+  },
+  {
+    id: 2,
+    author: 'DFX',
+    name: 'Relax Your Body (Remastered) - EP',
+    cover:
+      'https://z9j9e3w7.stackpathcdn.com/wp-content/uploads/2016/11/DFX-Art.jpg',
+  },
+  {
+    id: 3,
+    author: 'Puscifer',
+    name: 'The Underwhelming',
+    cover:
+      'https://img.discogs.com/VLRt7UDRk4Fy9TKvhudyvOnjg8Q=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-15931879-1600677638-5830.jpeg.jpg',
+  },
+  {
+    id: 4,
+    author: 'Soen',
+    name: 'Cognitive',
+    cover: 'https://m.media-amazon.com/images/I/81PcN3eQUTL._SS500_.jpg',
+  },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const AlbumList = () => (
+  <section className='albumlist'>
+    {albums.map((a) => (
+      <Album key={a.id} album={a} />
+    ))}
+  </section>
+)
+
+const Album = (props) => {
+  if (props.hasOwnProperty('album')) {
+    const { author, cover, name } = props.album
+    return (
+      <article className='album'>
+        <h1>{author.toUpperCase()}</h1>
+        <img
+          class='albumcover'
+          src={cover}
+          alt='album-cover'
+          onClick={() => alert('albumCoverClicked')}
+        />
+        <h5>{name}</h5>
+      </article>
+    )
+  } else {
+    return null
+  }
+}
+
+ReactDom.render(<AlbumList />, document.getElementById('root'))
